69385958c80a7cf1bb5325307227fb72cf789131
throw226th <haritharan98@siswa.um.edu.my>
2024-08-18 22:43:12 +0800

    10.14: storing the access token step1

---
+++ frontend/rate-repository-app/src/utils/authStorage.js
@@ -0,0 +1,24 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+class AuthStorage {
+    constructor(namespace = 'auth') {
+        this.namespace = namespace;
+    }
+
+    async getAccessToken() {
+        const token = await AsyncStorage.getItem(`${this.namespace}`);
+
+        return token ? JSON.parse(token) : [];
+    }
+
+    async setAccessToken(accessToken) {
+        const currentToken = await this.getAccessToken();
+        const newToken = [...currentToken, accessToken];
+
+        await AsyncStorage.setItem(`${this.namespace}`, JSON.stringify(newToken));
+    }
+
+    async removeAccessToken() {
+        await AsyncStorage.removeItem(`${this.namespace}`);
+    }
+}
